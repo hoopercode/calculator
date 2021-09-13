4035a4e81e9c1d5343a1d02094d12876dd249de8
hpteach2021 <hooper.rob@gmail.com>
2021-09-13 17:35:38 +0100

    main

---
+++ script/main.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll(".container__button")
+// const operationButtons = document.querySelectorAll('[data-operation]')
+// const equalsButton = document.querySelector('[data-equals]')
+// const allClearButton = document.querySelector('[data-all-clear]')
+// const deleteButton = document.querySelector('[data-delete]')
+
+console.log(numberButtons)
