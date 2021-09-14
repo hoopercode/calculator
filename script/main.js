904e649b46399ca6b5594871a0dd7506c55563d5
hpteach2021 <hooper.rob@gmail.com>
2021-09-14 13:38:12 +0100

    sorted CSS and started JS

--- script/main.js
+++ script/main.js
@@ -1,7 +1,22 @@
-const numberButtons = document.querySelectorAll(".container__button")
-// const operationButtons = document.querySelectorAll('[data-operation]')
-// const equalsButton = document.querySelector('[data-equals]')
-// const allClearButton = document.querySelector('[data-all-clear]')
-// const deleteButton = document.querySelector('[data-delete]')
+//Elements to use//
 
+
+const numberButtons = document.querySelectorAll("[data-number]")
+const operatorButtons = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals")
+const allClearButton = document.querySelector("[data-all-clear]")
+const backspaceButton = document.querySelector("[data-backspace]")
+const displayZone = document.querySelector("[data-current-display]")
+
+//Functions
 console.log(numberButtons)
+console.log(operatorButtons)
+console.log(equalsButton)
+console.log(allClearButton)
+console.log(backspaceButton)
+console.log(displayZone)
+
+
+class Calculator {
+  
+}
